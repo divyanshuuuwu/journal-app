c575856f36e8d3f022baf509d39d6b1e30410105
divyanshu <divyanshusah44@gmail.com>
2026-08-08 14:44:57 +0530

    register api working

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,13 +5,50 @@ const bcrypt = require("bcrypt")
 
 
 const registerUser = async (req, res) => {
-    const {username , email , passowrd , role="user"}= req.body
+    const {username , email , password , role="user"}= req.body
     
-const 
+const isUserAlreadyExists = await userModel.findOne({
+    $or:[
+        {username},
+        {email}
+    ]
+})
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+            message:"User Already exists"
+        })
+    }
 
+    const hashedPassword = await bcrypt.hash(password,10)
 
+        const user = await userModel.create({
+            username,
+            email,
+            passowrd:hashedPassword,
+            role
+        })
 
-
+        res.status(201).json({
+        message:"user registered sucessfully",
+        user:{
+            username:user.username,
+            email:user.email,
+            role:user.role                  
+        }
+    })
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {registerUser}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const authController = require("../controllers/auth.controller")
 
 
-
+router.post("/register", authController.registerUser)
 
 
 
